76fe0f7ded03aa7cedf0f645b814f8e8492f196b
isabella <isabella_aus_china@163.com>
2020-07-29 22:40:51 +0800

    Add comment and post

---
+++ backend/graminsta/post/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Comment(models.Model):
+    commentId = models.IntegerField(default=0)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    postId = models.IntegerField(default=0)
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Publisher_Of_Post")
+    time = models.DateTimeField(auto_now=True)
+    markedUser = models.ManyToManyField(User, related_name="User_Who_Marked_Post")
+    content = models.BinaryField()
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)

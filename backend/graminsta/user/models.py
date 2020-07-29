df1f24a7d74d11c01fb5887e05e7c163cccf5160
ZhengLiangfeng <i_am_shiyi@163.com>
2020-07-29 14:22:46 +0800

    modify User class into UserInfo class

--- backend/graminsta/user/models.py
+++ backend/graminsta/user/models.py
@@ -4,12 +4,15 @@
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class User(models.Model):
-    """User Class
+class UserInfo(models.Model):
+    """UserInfo Class
     """
-    user_name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    reg_time = models.DateTimeField(auto_now=True)
+    following_user = models.ManyToManyField("self", symmetrical=False)

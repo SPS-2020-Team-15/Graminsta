fce2628c9579b6a68a804f4c39f88c8a1f89c7f6
ZhengLiangfeng <i_am_shiyi@163.com>
2020-07-30 14:10:08 +0800

    modify age and gender data type && re-generate migration file

--- backend/graminsta/core/models.py
+++ backend/graminsta/core/models.py
@@ -12,5 +12,9 @@ class UserInfo(models.Model):
     """UserInfo Class
     """
     user = models.OneToOneField(auth_models.User, on_delete=models.CASCADE)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=10)
+    age = models.PositiveIntegerField()
+
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
+    gender = models.IntegerField(choices=GENDER_CHOICES)

3b31371c11f0e8ecf12744596a78586912d5f6d8
e <1447602704@qq.com>
2020-07-30 21:12:24 +0800

    Modify user import to pass CI

--- backend/graminsta/post/models.py
+++ backend/graminsta/post/models.py
@@ -1,22 +1,22 @@
 # -*- coding: UTF-8 -*-
 """models.py"""
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import models as auth_models
 
 
 class Post(models.Model):
     """A Post is usually user-generated which contains information
     like an image, and some description to the image"""
-    publisher = models.ForeignKey(User, on_delete=models.CASCADE,
+    publisher = models.ForeignKey(auth_models.User, on_delete=models.CASCADE,
                                   related_name="published_posts")
     description = models.TextField()
     img = models.ImageField(upload_to='img')
-    marked_users = models.ManyToManyField(User, related_name="marking_users")
+    marked_users = models.ManyToManyField(auth_models.User, related_name="marking_users")
 
 
 class Comment(models.Model):
     """A comment is usually user-generated under other user's post,
     which contains some text"""
-    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(auth_models.User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now=True)

7381b5f5f42098a1badb9aebfc66a1eaf093ece8
Liangfeng Zheng <liangfengzheng@buaa.edu.cn>
2020-07-30 21:28:56 +0800

    add user model (#1)
    * add user model
    
    * modify code to pass CI
    
    * modify User class into UserInfo class
    
    * add related name
    
    * move user into core
    
    * add migraation file
    
    * modify age and gender data type && re-generate migration file
    
    * fix nits

---
+++ backend/graminsta/core/apps.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+""" apps.py
+"""
+
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    """Default CoreConfig Class
+    """
+    name = 'core'
---
+++ backend/graminsta/core/models.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+"""models.py
+"""
+
+from django.db import models
+from django.contrib.auth import models as auth_models
+
+
+# Create your models here.
+class UserInfo(models.Model):
+    """UserInfo Class
+    """
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = ((GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female'))
+
+    user = models.OneToOneField(auth_models.User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)

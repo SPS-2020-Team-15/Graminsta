610cb92c33c84bb145a2bdf3d54d85f5e2b3c0c8
e <1447602704@qq.com>
2020-08-02 16:30:58 +0800

    Add post, comment and follow relationship

---
+++ backend/graminsta/post/utils.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""utils.py"""
+from django.contrib.auth import models as auth_models
+from .models import FollowRelationship
+
+
+def get_people_user_follows(user):
+    """
+    Returns a QuerySet representing the users that the given user follows.
+
+    """
+    ids = FollowRelationship.objects.filter(from_user=user)\
+        .values_list('to_user', flat=True)
+    return auth_models.User.objects.filter(id__in=ids)
+
+
+def get_people_following_user(user):
+    """
+    Returns a QuerySet representing the users that follow the given user.
+
+    """
+    ids = FollowRelationship.objects.filter(to_user=user)\
+        .values_list('from_user', flat=True)
+    return auth_models.User.objects.filter(id__in=ids)

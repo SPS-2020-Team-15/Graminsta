43cbf1039c09f0a5dd4f8e3a78d3328543b4ec91
ZhengLiangfeng <i_am_shiyi@163.com>
2020-08-06 18:45:46 +0800

    modify urls.py

--- backend/graminsta/core/urls.py
+++ backend/graminsta/core/urls.py
@@ -1,14 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 """
-Urls for  core app.
+Urls for core app.
 """
 
 from django.urls import path
-from .views import UserInfoRecordView
+from core.views import UserInfoRecordView
 
 
-app_name = 'core'
 urlpatterns = [
     path('user/', UserInfoRecordView.as_view(), name='user'),
 ]

d0cdcec7bca4682177ba8d2d4ce54670a2cca871
isabella <isabella_aus_china@163.com>
2020-08-07 23:41:33 +0800

    Replace string array with enum

--- flutter/Graminsta/lib/createPostWidget.dart
+++ flutter/Graminsta/lib/createPostWidget.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
+enum AccessControlType {
+  public,
+  private,
+  visibleToSpecificFriends,
+  invisibleToSpecificFriends,
+}
+
+extension AccessControlTypeExtension on AccessControlType {
+  String toHumanReadableString() {
+    switch (this) {
+      case AccessControlType.public:
+        return "All";
+      case AccessControlType.private:
+        return "Private";
+      case AccessControlType.visibleToSpecificFriends:
+        return "Visible to specfic friends";
+      case AccessControlType.invisibleToSpecificFriends:
+        return "Invisible to specfic friends";
+      default:
+        return null;
+    }
+  }
+}
+
 class CreatePostWidget extends StatefulWidget {
   CreatePostWidget({Key key}) : super(key: key);
 
@@ -8,27 +32,27 @@ class CreatePostWidget extends StatefulWidget {
 }
 
 class _CreatePostWidgetState extends State<CreatePostWidget> {
-  String accessSetting = "All";
+  AccessControlType accessControl = AccessControlType.public;
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-
     double height = MediaQuery.of(context).size.height;
 
     return new Scaffold(
-        appBar: AppBar(title: Text('Create a Post'), actions: <Widget>[
+      appBar: AppBar(
+        title: Text('Create a Post'),
+        actions: <Widget>[
           IconButton(
-            icon: Icon(
-              Icons.save,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              // do something
-            },
-          )
-        ]),
-        body: new Column(children: <Widget>[
+              icon: Icon(
+                Icons.save,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      body: new Column(
+        children: <Widget>[
           Container(
             margin: EdgeInsets.all(8.0),
             padding: EdgeInsets.only(bottom: 40.0),
@@ -41,54 +65,65 @@ class _CreatePostWidgetState extends State<CreatePostWidget> {
             ),
           ),
           Container(
-              margin: EdgeInsets.only(left: width / 20),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                      height: width / 4,
-                      width: width / 4,
-                      color: Color.fromRGBO(220, 220, 220, 0.8),
-                      child: Icon(Icons.add_a_photo)))),
+            margin: EdgeInsets.only(left: width / 20),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                height: width / 4,
+                width: width / 4,
+                color: Color.fromRGBO(220, 220, 220, 0.8),
+                child: Icon(Icons.add_a_photo),
+              ),
+            ),
+          ),
           Container(
-              margin: EdgeInsets.only(top: height / 10),
-              child: new Column(
-                children: <Widget>[
-                  new ListTile(
-                      leading: const Icon(Icons.location_city),
-                      title: Text("Location"),
-                      trailing: Icon(Icons.arrow_right, size: 30)),
-                  new ListTile(
-                    leading: const Icon(Icons.alternate_email),
-                    title: Text("Mention"),
-                    trailing: Icon(Icons.arrow_right, size: 30),
+            margin: EdgeInsets.only(top: height / 10),
+            child: new Column(
+              children: <Widget>[
+                new ListTile(
+                  leading: const Icon(Icons.location_city),
+                  title: Text("Location"),
+                  trailing: Icon(Icons.arrow_right, size: 30),
+                ),
+                new ListTile(
+                  leading: const Icon(Icons.alternate_email),
+                  title: Text("Mention"),
+                  trailing: Icon(Icons.arrow_right, size: 30),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: Text("Share With"),
+                  trailing: DropdownButton<AccessControlType>(
+                    value: accessControl,
+                    icon: Icon(Icons.arrow_downward),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (AccessControlType newValue) {
+                      setState(() {
+                        accessControl = newValue;
+                      });
+                    },
+                    items: <AccessControlType>[
+                      AccessControlType.public,
+                      AccessControlType.private,
+                      AccessControlType.visibleToSpecificFriends,
+                      AccessControlType.invisibleToSpecificFriends
+                    ].map<DropdownMenuItem<AccessControlType>>(
+                        (AccessControlType accessControlType) {
+                      return DropdownMenuItem<AccessControlType>(
+                        value: accessControlType,
+                        child: Text(accessControlType.toHumanReadableString()),
+                      );
+                    }).toList(),
                   ),
-                  ListTile(
-                      leading: const Icon(
-                        Icons.person,
-                      ),
-                      title: Text("Share With"),
-                      trailing: DropdownButton<String>(
-                        value: accessSetting,
-                        icon: Icon(Icons.arrow_downward),
-                        underline: Container(
-                          height: 2,
-                          color: Colors.deepPurpleAccent,
-                        ),
-                        onChanged: (String newValue) {
-                          setState(() {
-                            accessSetting = newValue;
-                          });
-                        },
-                        items: <String>['All', 'Private', 'Friends']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ))
-                ],
-              ))
-        ]));
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

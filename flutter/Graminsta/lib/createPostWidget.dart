67cf0ac90bcb8d03e96c04ed16d2eb6df3dcdac9
isabella <isabella_aus_china@163.com>
2020-08-07 12:00:18 +0800

    Add static page createPostWidget

---
+++ flutter/Graminsta/lib/createPostWidget.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class CreatePostWidget extends StatefulWidget {
+  CreatePostWidget({Key key}) : super(key: key);
+
+  @override
+  _CreatePostWidgetState createState() => _CreatePostWidgetState();
+}
+
+class _CreatePostWidgetState extends State<CreatePostWidget> {
+  String accessSetting = "All";
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    double height = MediaQuery.of(context).size.height;
+
+    return new Scaffold(
+        appBar: AppBar(title: Text('Create a Post'), actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.save,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ]),
+        body: new Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(8.0),
+            padding: EdgeInsets.only(bottom: 40.0),
+            child: TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: "Describe your post",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(left: width / 20),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      height: width / 4,
+                      width: width / 4,
+                      color: Color.fromRGBO(220, 220, 220, 0.8),
+                      child: Icon(Icons.add_a_photo)))),
+          Container(
+              margin: EdgeInsets.only(top: height / 10),
+              child: new Column(
+                children: <Widget>[
+                  new ListTile(
+                      leading: const Icon(Icons.location_city),
+                      title: Text("Location"),
+                      trailing: Icon(Icons.arrow_right, size: 30)),
+                  new ListTile(
+                    leading: const Icon(Icons.alternate_email),
+                    title: Text("Mention"),
+                    trailing: Icon(Icons.arrow_right, size: 30),
+                  ),
+                  ListTile(
+                      leading: const Icon(
+                        Icons.person,
+                      ),
+                      title: Text("Share With"),
+                      trailing: DropdownButton<String>(
+                        value: accessSetting,
+                        icon: Icon(Icons.arrow_downward),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            accessSetting = newValue;
+                          });
+                        },
+                        items: <String>['All', 'Private', 'Friends']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ))
+                ],
+              ))
+        ]));
+  }
+}

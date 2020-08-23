fec6c6ec993cd776279944e0de67c6ee01243868
isabella <isabella_aus_china@163.com>
2020-08-23 12:30:19 +0800

    Restore main.dart

--- flutter/Graminsta/lib/main.dart
+++ flutter/Graminsta/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Graminsta",
-      initialRoute: '/create',
+      initialRoute: '/home',
       routes: {
         '/create': (context) => CreatePostWidget(),
         '/mention': (context) => MentionUserWidget(),
